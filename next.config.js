67255bb27dc5ad734871c8a7da9175618134ed3d
gusborba9-star <gusborba9@gmail.com>
2026-06-17 21:28:16 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Isso garante que o projeto rode em modo Serverless compatível com Vercel
+  output: 'standalone', 
+};
+
+module.exports = nextConfig;
